63982cf04ee9884c42c78bd359d361365c9a230f
Jesús García <ctw@ctwhome.com>
2021-09-03 12:00:29 +0200

    first

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  <div class="btn">Daisy Button doesn't seem to work</div>
+  <button class="text-red-500 mb-[200px] !text-green-300">Autoinfer</button>
+
+  <p>Value Auto-infer doesn't work</p>
+
+  <Tutorial/>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
